1465c308ebbb42ad237fa41f3cfd24992ed2966c
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-11-03 16:15:56 -0700

    Refactored Logo as a component/exported

---
+++ src/components/Logo.js
@@ -0,0 +1,9 @@
+import img from '../assets/images/logo.svg';
+
+import React from 'react';
+
+const Logo = () => {
+  return <img src={img} alt="jobster logo" className="logo" />;
+};
+
+export default Logo;
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,12 +1,12 @@
-import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
 import LandingPage from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
 
 const Landing = () => {
   return (
     <LandingPage>
       <nav>
-        <img src={logo} alt="jobster logo" className="logo" />
+        <Logo />
       </nav>
       <div className="container page">
         <div className="info">
