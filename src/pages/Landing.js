61fdd5e44614b50cdb64980c1210c317c4f6fd30
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-11-03 15:06:36 -0700

    Added assets to Landing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,7 +2,29 @@ import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
 
 const Landing = () => {
-  return <h1>Landing</h1>;
+  return (
+    <main>
+      {/* NAV */}
+      <nav>
+        <img src={logo} alt="jobster log" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* HERO */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam omnis
+            officiis amet animi aliquam quia neque hic obcaecati impedit
+            ducimus.
+          </p>
+          <button class="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
 };
 
 export default Landing;
