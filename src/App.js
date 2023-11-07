9cb9564f17396317c0aaed3754e92c5217b9042c
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-11-07 10:46:53 -0800

    Refactored Dashboard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import { Landing, Dashboard, Error, Register } from './pages';
+import { Landing, Error, Register } from './pages';
+import {
+  Profile,
+  AddJob,
+  AllJobs,
+  Stats,
+  SharedLayout,
+} from './pages/dashboard';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -8,7 +15,12 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Stats />} />
+            <Route path="all-jobs" element={<AllJobs />} />
+            <Route path="add-job" element={<AddJob />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
           <Route path="landing" element={<Landing />} />
           <Route path="register" element={<Register />} />
           <Route path="*" element={<Error />} />
