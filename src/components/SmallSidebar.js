eecf5628ec6b2f5e4c1902b66a8df3bb76ebac32
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-11-09 12:12:15 -0800

    Populated data

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,9 +1,10 @@
 import Wrapper from '../assets/wrappers/SmallSidebar';
 import { FaTimes } from 'react-icons/fa';
-import { NavLink } from 'react-router-dom';
 import Logo from './Logo';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleSidebar } from '../features/user/userSlice';
+import { NavLink } from 'react-router-dom';
+import links from '../utils/links';
 
 const SmallSidebar = () => {
   const { isSidebarOpen } = useSelector((store) => store.user);
@@ -27,7 +28,25 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className="nav-links">nav links</div>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { text, path, id, icon } = link;
+              return (
+                <NavLink
+                  to={path}
+                  className={({ isActive }) => {
+                    return isActive ? 'nav-link active' : 'nav-link';
+                  }}
+                  key={id}
+                  onClick={toggle}
+                  end
+                >
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
         </div>
       </div>
     </Wrapper>
