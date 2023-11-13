154ac73558127d91dca7c07ceedbed46acd35544
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-11-13 13:01:35 -0800

    Add job/clear data

--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -2,7 +2,11 @@ import { FormRow, FormRowSelect } from '../../components';
 import Wrapper from '../../assets/wrappers/DashboardFormPage';
 import { useSelector, useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
-import { handleChange, clearValues } from '../../features/job/jobSlice';
+import {
+  handleChange,
+  clearValues,
+  createJob,
+} from '../../features/job/jobSlice';
 
 const AddJob = () => {
   const {
@@ -24,7 +28,9 @@ const AddJob = () => {
 
     if (!position || !company || !jobLocation) {
       toast.error('Please fill out all fields');
+      return;
     }
+    dispatch(createJob({ position, company, jobLocation, jobType, status }));
   };
 
   const handleJobInput = (e) => {
