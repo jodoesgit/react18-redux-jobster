bbd523d873affd32d0e6273dc749bcb6df72c4f9
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-11-29 18:15:11 -0800

    Delete job, update listings

--- src/components/Job.js
+++ src/components/Job.js
@@ -4,6 +4,7 @@ import Wrapper from '../assets/wrappers/Job';
 import { useDispatch } from 'react-redux';
 import JobInfo from './JobInfo';
 import moment from 'moment/moment';
+import { deleteJob } from '../features/job/jobSlice';
 
 const Job = ({
   _id,
@@ -46,7 +47,7 @@ const Job = ({
             </Link>
             <button
               className="btn delete-btn"
-              onClick={() => console.log('delete job')}
+              onClick={() => dispatch(deleteJob(_id))}
             >
               delete
             </button>
--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -39,6 +39,25 @@ export const createJob = createAsyncThunk(
   }
 );
 
+export const deleteJob = createAsyncThunk(
+  'job/deleteJob',
+  async (jobId, thunkAPI) => {
+    thunkAPI.dispatch(showLoading());
+    try {
+      const resp = await customFetch.delete(`/jobs/${jobId}`, {
+        headers: {
+          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+        },
+      });
+      thunkAPI.dispatch(getAllJobs());
+      return resp.data;
+    } catch (error) {
+      thunkAPI.dispatch(hideLoading());
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
+
 const jobSlice = createSlice({
   name: 'job',
   initialState,
