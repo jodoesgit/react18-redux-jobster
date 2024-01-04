36371e0afb7874f841a929cdbb057ab621288be1
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2024-01-03 17:19:05 -0800

    Created & rendered stats/chart components

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
-import Logo from './Logo';
-import FormRow from './FormRow';
-import Navbar from './Navbar';
-import BigSidebar from './BigSidebar';
-import SmallSidebar from './SmallSidebar';
-import FormRowSelect from './FormRowSelect';
-import JobsContainer from './JobsContainer';
-import SearchContainer from './SearchContainer';
+import Logo from "./Logo";
+import FormRow from "./FormRow";
+import Navbar from "./Navbar";
+import BigSidebar from "./BigSidebar";
+import SmallSidebar from "./SmallSidebar";
+import FormRowSelect from "./FormRowSelect";
+import JobsContainer from "./JobsContainer";
+import SearchContainer from "./SearchContainer";
+import StatsContainer from "./StatsContainer";
+import ChartsContainer from "./ChartsContainer";
 
 export {
   Logo,
@@ -16,4 +18,6 @@ export {
   FormRowSelect,
   JobsContainer,
   SearchContainer,
+  StatsContainer,
+  ChartsContainer,
 };
