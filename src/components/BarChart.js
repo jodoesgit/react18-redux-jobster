81fa38a17d1ba27c7ddb112d230dfb9b85ef193d
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2024-01-06 12:53:16 -0800

    Added charts to project

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,8 +1,25 @@
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
 
-const BarChart = () => {
+const BarChartComponent = ({ data }) => {
   return (
-    <div>BarChart</div>
-  )
-}
+    <ResponsiveContainer width='100%' height={300}>
+      <BarChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray='10 10 ' />
+        <XAxis dataKey='date' />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Bar dataKey='count' fill='#3b82f6' barSize={75} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
 
-export default BarChart
+export default BarChartComponent;
